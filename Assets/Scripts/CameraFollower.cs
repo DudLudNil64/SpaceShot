8e4baa768e7a945da5ea9feea4f7e0de1e202af5
DudLudNil64 <ludwig.gothbergmeiton@elev.ga.lbs.se>
2025-10-14 11:54:43 +0200

    Started On The Players
    Also Removed README
    And The Player Change Is Glow And Particles

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Vector3 CameraOffset;
+    public GameObject Player;
+
+    private void Update()
+    {
+
+    }
+}
